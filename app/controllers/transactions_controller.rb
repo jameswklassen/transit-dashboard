130fbbb2b743c2d972eb3f12b3cb7298259756fb
James Klassen <jameswklassen@gmail.com>
2019-11-16 16:30:21 -0500

    Allow transaction creation

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,11 +1,40 @@
 # frozen_string_literal: true
 
 class TransactionsController < ApplicationController
+  def new
+    @transaction = Transaction.new
+  end
+
+  def create
+    if Transaction.new(transaction_params).save
+      flash[:notice] = 'created'
+      redirect_to(transactions_path)
+    else
+      flash[:error] = 'Error creating transaction.'
+    end
+  end
+
   def index
     @transactions = Transaction.all
   end
 
   def show
     @transaction = Transaction.find(params[:id])
+  end
+
+  private
+
+  def transaction_params
+    params.require(:transaction).permit(
+      :date,
+      :transaction_number,
+      :transit_agency,
+      :location,
+      :transaction_type,
+      :service_class,
+      :discount,
+      :amount,
+      :balance
+    )
   end
 end

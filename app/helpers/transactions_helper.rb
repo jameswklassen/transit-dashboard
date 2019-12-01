abbe8192269648b4703dbc3e3f6d5c1c5919d66b
James Klassen <jameswklassen@gmail.com>
2019-11-30 20:01:22 -0500

    Add some data helpers

--- app/helpers/transactions_helper.rb
+++ app/helpers/transactions_helper.rb
@@ -1,4 +1,19 @@
 # frozen_string_literal: true
 
 module TransactionsHelper
+  extend self
+  
+  def average_time(transactions)
+    hours, minutes = [], []
+
+    count = transactions.pluck(:date).each do |date| 
+      hours << date.localtime.hour
+      minutes << date.localtime.min
+    end.count
+
+    average_hour = hours.sum / count
+    average_minute =  minutes.sum / count
+
+    Time.now.change({ hour: average_hour, min: average_minute })
+  end
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,4 +2,12 @@
 
 class Transaction < ApplicationRecord
   belongs_to :user
+  
+  scope :morning, -> {
+    where('extract(hour from date) between 0 and 11')
+  }
+
+  scope :afternoon, -> {
+    where('extract(hour from date) between 12 and 23')
+  }
 end

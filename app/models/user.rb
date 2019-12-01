dad3b85c7e89c23a5e7a0cc53cbe1f64c0efd90a
James Klassen <jameswklassen@gmail.com>
2019-12-01 17:28:30 -0500

    Add delete cascading for user's transactions

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
-  has_many :transactions
+  has_many :transactions, dependent: :destroy
 end
